8981374671adf9aa4c48da73f9af20fdb3bf0841
ingof <ems-gateway@arcor.de>
2016-05-07 10:34:30 +0200

    Update calcemsgraphs.sh

--- scripts/calcemsgraphs.sh
+++ scripts/calcemsgraphs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. /emsincludes/config.sh
+. ../emsincludes/config.sh
 $BINDIR/ems-gen-graphs.py $GRAPHDIR hour > /dev/null 2>&1
 $BINDIR/ems-gen-graphs.py $GRAPHDIR day > /dev/null 2>&1
 $BINDIR/ems-gen-graphs.py $GRAPHDIR halfweek > /dev/null 2>&1
