e54843f435136e045febef7e7151f5c7f562abb4
ingof <ems-gateway@arcor.de>
2016-05-22 02:30:34 +0200

    Update config.py

--- includes/config.py
+++ includes/config.py
@@ -3,7 +3,7 @@ mysql_socket_path = "/var/run/mysql/mysqld.sock"
 mysql_user = "ems"
 mysql_password = "buderusems"
 mysql_db_name = "ems_data"
-graphtargetpath = "/volume1/web/ems-front/graphs/"
+graphtargetpath = "/usr/syno/etc/www/ems-front/graphs/"
 
 # Moosys frontend:
 #mysql_socket_path = "/var/run/mysql/mysqld.sock"
