20dbdb6e04df05b7af5686441c8c84073c0ced9f
ingof <ems-gateway@arcor.de>
2016-05-25 13:57:50 +0200

    Update config.py

--- includes/config.py
+++ includes/config.py
@@ -1,12 +1,12 @@
 # Diskstation with DSM 6.0
-mysql_socket_path = "/var/run/mysql/mysqld.sock"
+mysql_socket_path = "/var/run/mysqld/mysqld.sock"
 mysql_user = "ems"
 mysql_password = "buderusems"
 mysql_db_name = "ems_data"
-graphtargetpath = "/usr/syno/etc/www/ems-front/graphs/"
+graphtargetpath = "/volume1/web/ems-front/graphs/"
 
 # Moosys frontend:
-#mysql_socket_path = "/var/run/mysql/mysqld.sock"
+#mysql_socket_path = "/var/run/mysqld/mysqld.sock"
 #mysql_user = "ems"
 #mysql_password = "buderusems"
 #mysql_db_name = "ems_data"
