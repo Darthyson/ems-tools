4556d910de02bc0903a4e717aa1485c46aeea7a6
ingof <ems-gateway@arcor.de>
2016-05-25 13:56:00 +0200

    Update config.sh

--- includes/config.sh
+++ includes/config.sh
@@ -1,6 +1,6 @@
 # Diskstation with DSM 6.0
-BINDIR="/var/local/ems-tools/scripts/"
-GRAPHDIR="/usr/syno/etc/www/ems-front/graphs/"
+BINDIR="/var/local/ems-tools/scripts"
+GRAPHDIR="/volume1/web/ems-front/graphs/"
 
 # Moosys Frontend:
 #BINDIR="/home/programming/Heizung/ems-tools/scripts/"
