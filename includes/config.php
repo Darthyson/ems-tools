6d65a2fb46f1f189be2b7aa58a7f291bba2fb246
ingof <ems-gateway@arcor.de>
2016-11-17 13:49:42 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,20 +1,20 @@
 <?php
 # Diskstation with DSM 6.0
-$min_collector_version=2016030701;
+$min_collector_version = 2016030701;
 $mysql_host = "localhost";
-$mysql_user= "ems";
+$mysql_user = "ems";
 $mysql_pass = "buderusems";
 $graphpath = "/volume1/web/ems-front/graphs/";
 $emsscriptpath = "/var/local/ems-tools/scripts";
-$tmpdir="/volume1/@tmp/";
-$html_cols=3;
+$tmpdir = "/volume1/@tmp/";
+$html_cols = 3;
 
 # moosys frontend:
-#$min_collector_version=2016030701;
+#$min_collector_version = 2016030701;
 #$mysql_host = "localhost";
-#$mysql_user= "ems";
+#$mysql_user = "ems";
 #$mysql_pass = "buderusems";
 #$graphpath = "/home/htdocs/moosy.net/www/graphs/";
 #$emsscriptpath = "/home/programming/Heizung/ems-tools/scripts/";
-#$tmpdir="/tmp/";
-#$html_cols=3;
+#$tmpdir = "/tmp/";
+#$html_cols = 3;
